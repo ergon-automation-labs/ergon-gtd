5c510b5f86e61214c32b04ce6f4dde82a54a42e8
Abby Malson <august.malson@gmail.com>
2026-04-21 03:49:06 -0600

    Fix migrations to handle tables created in different order

--- priv/repo/migrations/20260404000001_enforce_tenant_not_null.exs
+++ priv/repo/migrations/20260404000001_enforce_tenant_not_null.exs
@@ -1,15 +1,23 @@
 defmodule BotArmyGtd.Repo.Migrations.EnforceTenantNotNull do
   use Ecto.Migration
+  require Logger
 
   def up do
     for table <- [:projects, :tasks, :inbox_items, :decompositions, :log_entries] do
-      execute("ALTER TABLE #{table} ALTER COLUMN tenant_id SET NOT NULL")
+      if table_exists?(table) do
+        execute("ALTER TABLE #{table} ALTER COLUMN tenant_id SET NOT NULL")
+        Logger.info("Enabled NOT NULL constraint on #{table}.tenant_id")
+      else
+        Logger.info("Skipping #{table}.tenant_id - table not yet created")
+      end
     end
   end
 
   def down do
     for table <- [:projects, :tasks, :inbox_items, :decompositions, :log_entries] do
-      execute("ALTER TABLE #{table} ALTER COLUMN tenant_id DROP NOT NULL")
+      if table_exists?(table) do
+        execute("ALTER TABLE #{table} ALTER COLUMN tenant_id DROP NOT NULL")
+      end
     end
   end
 end
