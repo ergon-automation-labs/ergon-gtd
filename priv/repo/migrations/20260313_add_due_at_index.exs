2d0596a386674d6e8a2d2868481bd39b1addc237
Abby Malson <august.malson@gmail.com>
2026-04-21 06:33:17 -0600

    Simplify migrations - use change/0 and drop database

--- priv/repo/migrations/20260313_add_due_at_index.exs
+++ priv/repo/migrations/20260313_add_due_at_index.exs
@@ -1,28 +1,8 @@
 defmodule BotArmyGtd.Repo.Migrations.AddDueAtIndex do
   use Ecto.Migration
-  import Ecto.Migration, only: [table_exists?: 1]
-  require Logger
 
-  def up do
-    # Add index on due_at for efficient queries in ReviewScheduler
-    # Queries: decompositions with status="completed" and due_at <= now
-
-    # Check if table exists first (handles case where migrations ran out of order)
-    if table_exists?(:decompositions) do
-      create(index(:decompositions, [:due_at]))
-
-      # Composite index for optimized filtering by status and due_at
-      create(index(:decompositions, [:status, :due_at]))
-    else
-      Logger.info("Skipping add_due_at_index - decompositions table not yet created")
-    end
-  end
-
-  def down do
-    # Only drop if table exists
-    if table_exists?(:decompositions) do
-      drop(index(:decompositions, [:due_at]))
-      drop(index(:decompositions, [:status, :due_at]))
-    end
+  def change do
+    create(index(:decompositions, [:due_at]))
+    create(index(:decompositions, [:status, :due_at]))
   end
 end
--- priv/repo/migrations/20260404000001_enforce_tenant_not_null.exs
+++ priv/repo/migrations/20260404000001_enforce_tenant_not_null.exs
@@ -1,24 +1,15 @@
 defmodule BotArmyGtd.Repo.Migrations.EnforceTenantNotNull do
   use Ecto.Migration
-  import Ecto.Migration, only: [table_exists?: 1]
-  require Logger
 
-  def up do
+  def change do
     for table <- [:projects, :tasks, :inbox_items, :decompositions, :log_entries] do
-      if table_exists?(table) do
-        execute("ALTER TABLE #{table} ALTER COLUMN tenant_id SET NOT NULL")
-        Logger.info("Enabled NOT NULL constraint on #{table}.tenant_id")
-      else
-        Logger.info("Skipping #{table}.tenant_id - table not yet created")
-      end
+      execute("ALTER TABLE #{table} ALTER COLUMN tenant_id SET NOT NULL")
     end
   end
 
   def down do
     for table <- [:projects, :tasks, :inbox_items, :decompositions, :log_entries] do
-      if table_exists?(table) do
-        execute("ALTER TABLE #{table} ALTER COLUMN tenant_id DROP NOT NULL")
-      end
+      execute("ALTER TABLE #{table} ALTER COLUMN tenant_id DROP NOT NULL")
     end
   end
 end
