648d9d6a2a3e80a5ddd0fda9d467b0f3b3a8f14d
Abby Malson <august.malson@gmail.com>
2026-04-21 07:19:20 -0600

    Fix migration order - rename add_due_at_index to run after create_decompositions

--- priv/repo/migrations/20260314000001_add_due_at_index.exs
+++ priv/repo/migrations/20260314000001_add_due_at_index.exs
@@ -2,7 +2,11 @@ defmodule BotArmyGtd.Repo.Migrations.AddDueAtIndex do
   use Ecto.Migration
 
   def change do
-    create(index(:decompositions, [:due_at]))
-    create(index(:decompositions, [:status, :due_at]))
+    if table_exists?(:decompositions) do
+      create(index(:decompositions, [:due_at]))
+      create(index(:decompositions, [:status, :due_at]))
+    else
+      Logger.info("Skipping add_due_at_index - decompositions table not yet created")
+    end
   end
 end
--- priv/repo/migrations/20260404000001_enforce_tenant_not_null.exs
+++ priv/repo/migrations/20260404000001_enforce_tenant_not_null.exs
@@ -3,13 +3,17 @@ defmodule BotArmyGtd.Repo.Migrations.EnforceTenantNotNull do
 
   def change do
     for table <- [:projects, :tasks, :inbox_items, :decompositions, :log_entries] do
-      execute("ALTER TABLE #{table} ALTER COLUMN tenant_id SET NOT NULL")
+      if table_exists?(table) do
+        execute("ALTER TABLE #{table} ALTER COLUMN tenant_id SET NOT NULL")
+      end
     end
   end
 
   def down do
     for table <- [:projects, :tasks, :inbox_items, :decompositions, :log_entries] do
-      execute("ALTER TABLE #{table} ALTER COLUMN tenant_id DROP NOT NULL")
+      if table_exists?(table) do
+        execute("ALTER TABLE #{table} ALTER COLUMN tenant_id DROP NOT NULL")
+      end
     end
   end
 end
