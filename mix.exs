d30eb922396cee56f21dfa698a7b89c8cc25531b
Abby Malson <august.malson@gmail.com>
2026-07-16 14:38:26 -0600

    test: v0.7.186 - cleanup always exits 0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BotArmyGtd.MixProject do
   def project do
     [
       app: :bot_army_gtd,
-      version: "0.7.185",
+      version: "0.7.186",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
