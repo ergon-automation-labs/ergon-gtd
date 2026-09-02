b84b040dfb0c19135a96f26d80509abf81bf2b7e
Abby Malson <august.malson@gmail.com>
2026-09-03 01:26:52 +0300

    chore: Bump version to 0.7.223 to pick up runtime v0.14.70

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BotArmyGtd.MixProject do
   def project do
     [
       app: :bot_army_gtd,
-      version: "0.7.222",
+      version: "0.7.223",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
